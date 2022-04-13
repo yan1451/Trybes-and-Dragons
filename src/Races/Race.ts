44ed9c2b56f549c395dfd660d5ba386e0f8329c9
Yan Brasileiro <yan.brasileiro@gmail.com>
2022-04-13 19:53:40 -0300

    Requisito 1 , Created Class Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,11 @@
+export default abstract class Race {
+  constructor(private _name: string, private _dexterity: number) { }
+
+  get name() { return this._name; }
+  get dexterity() { return this._dexterity; }
+  abstract get maxLifePoints(): number;
+
+  static createdRacesInstances(): void {
+    throw new Error('Not implemented');    
+  }
+}
