a4fad14a0dc3484d59a76bd8118e6d74e75cc7c6
Yan Brasileiro <yan.brasileiro@gmail.com>
2022-04-13 19:55:28 -0300

    Requisito 4, class Archetype

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,20 @@
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetype {
+  private _special: number;
+  private _cost: number;
+
+  constructor(private _name: string) {
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  get name() { return this._name; }
+  get special() { return this._special; }
+  get cost() { return this._cost; }
+  abstract get energytype(): EnergyType;
+
+  static createdArchetypeInstances(): void {
+    throw new Error('Not implement');    
+  }
+}
